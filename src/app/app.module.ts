035d5097691057a13b0581dbf7e588050f943e12
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-11 18:05:22 +0300

    generated search form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { GitFormComponent } from './git-form/git-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    GitFormComponent
   ],
   imports: [
     BrowserModule,
