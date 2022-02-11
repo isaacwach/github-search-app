d0eceaad7dc231731cb9faf2c52163576e32c786
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-11 18:10:41 +0300

    generated users component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { GitFormComponent } from './git-form/git-form.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    GitFormComponent
+    GitFormComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
