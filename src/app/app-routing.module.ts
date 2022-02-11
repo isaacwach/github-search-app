092e127103dbe286054df1ea6d09dc5a223a8c97
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-11 19:04:56 +0300

    installed fontawesome and linked it in css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { RepositoryComponent } from './repository/repository.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
   { path: 'users', component: UsersComponent },
   { path: 'repository', component: RepositoryComponent },
 ];
