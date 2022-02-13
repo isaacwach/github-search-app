5714a91978f546b91fd1c0eb899083f564a67234
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-02-13 11:54:53 +0300

    added a card to display profile details

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -10,7 +10,7 @@ import {UserService} from '../user.service'
 export class UsersComponent implements OnInit {
   
 //profile: any[];
-profile: {name: string; followers: number; login: string; company: string;}
+profile: {name: string; followers: number; login: string; company: string; avatar_url: string;}
   constructor(private userService: UserService) {
     console.log("heey")
     this.userService.getProfileInfo().subscribe((profile =>{
